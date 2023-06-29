17235f72733fd271b7a38bbe7d9f73a96b120f98
Devansh Panirwala <dpanirwala3@gatech.edu>
2023-06-29 17:03:52 -0400

    first commit

---
+++ ExampleCMS/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace ExampleCMS.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
+
